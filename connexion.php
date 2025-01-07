b84343e15a3b9dc407d8b60e1f21fae0007d8c14
Michael ESSOMBA <messomba700@gmail.com>
2025-01-07 12:03:04 +0100

    Modification du css du fichier de connexion

--- connexion.php
+++ connexion.php
@@ -37,8 +37,9 @@ $conn = null;
         <meta http-equiv='X-UA-Compatible' content='IE=edge'>
         <title>Connexion et inscription </title>
         <meta name='viewport' content='width=device-width, initial-scale=1'>
-        <link rel='stylesheet' type='text/css' media='screen' href='css/bootstrap.css'>
-        <link rel='stylesheet' type='text/css' media='screen' href='css/all.min.css'>
+        <link rel='stylesheet' type='text/css' media='screen' href='css/connexion.css'>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
 
     </head>
 
@@ -89,4 +90,3 @@ $conn = null;
     </body>
 
     </html>
-    <script src='js/bootstrap.js'></script>
