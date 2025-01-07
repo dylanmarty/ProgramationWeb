780d963277db15927c471900458cdb0bb8a453de
Michael ESSOMBA <messomba700@gmail.com>
2025-01-07 21:51:59 +0100

    Presision de la methode du formulaire 'POST' et ajout des conditions de valdation d'un comptes liées à la taille  d'un nom ou d'un prenom et celle liées à l'unicité d'une adresse email

--- inscription.php
+++ inscription.php
@@ -7,18 +7,37 @@ $password = "";
 $base_name = "jeux_videos";
 
 //connexion base de données:
-$conn = new PDO("mysql:host=$host;dbname=$base_name", $username, $password);
+$connexion = new PDO("mysql:host=$host;dbname=$base_name", $username, $password);
 
 if (isset($_POST['valider'])) {
-    if (!empty($_POST['nom']) && !empty($_POST['prenom']) && !empty($_POST['email']) && !empty($_POST['password'])) {
-        $nom = $_POST['nom'];
-        $prenom = $_POST['prenom'];
-        $email = $_POST['email'];
-        $mdp = $_POST['password'];
+    if (!empty($_POST['nom']) and !empty($_POST['prenom']) and !empty($_POST['email']) and !empty($_POST['password'])) {
+        $nom = htmlspecialchars($_POST['nom']);
+        $prenom = htmlspecialchars($_POST['prenom']);
+        $email = htmlspecialchars($_POST['email']);
+        $mdp = sha1($_POST['password']);
+
+        if (strlen($nom) > 50 || strlen($prenom) > 50) {
+            $message = "Entrer un nom ou un prenom plus court";
+        } else {
+            $testemail = $connexion->prepare("SELECT * FROM users WHERE email=?");
+            $testemail->execute(array($email));
+
+            $controlemail = $testemail->rowCount();
+
+            if ($controlemail == 0) {
+                $insertion = $connexion->prepare("INSERT INTO users(nom,prenom,email,password) VALUES(?,?,?,?)");
+                $insertion->execute(array($nom, $prenom, $email, $mdp));
+                $message = "Votre compte a bien été crée";
+            } else {
+                echo "Désolé mais cette adresse est associé à un compte existant";
+            }
+        }
+    } else {
+        $message = "Remplisser tous les champs";
     }
 }
 
-$conn = null;
+$connexion = null;
 ?>
 <!DOCTYPE html>
 <html lang="fr">
@@ -28,8 +47,9 @@ $conn = null;
     <meta http-equiv='X-UA-Compatible' content='IE=edge'>
     <title>Connexion et inscription </title>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
-    <link rel='stylesheet' type='text/css' media='screen' href='css/bootstrap.css'>
-    <link rel='stylesheet' type='text/css' media='screen' href='css/all.min.css'>
+    <link rel='stylesheet' type='text/css' media='screen' href='css/inscription.css'>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 
 </head>
 
@@ -40,37 +60,46 @@ $conn = null;
                 <h2 class="text-center"> Inscription</h2>
                 <p class="text-center text-muted lead"> Simple et Rapide </p>
 
-                <form action="">
+                <form action="" method="POST">
                     <div class="input-group  mb-3">
                         <span class="input-group-text">
                             <i class="fa fa-user">
                             </i>
                         </span>
-                        <input type="text" class="form-control" placeholder="Nom ">
+                        <input type="text" class="form-control" name="nom" placeholder="Nom ">
                     </div>
                     <div class="input-group  mb-3">
                         <span class="input-group-text">
                             <i class="fa fa-user">
                             </i>
                         </span>
-                        <input type="text" class="form-control" placeholder="Prénom ">
+                        <input type="text" class="form-control" name="prenom" placeholder="Prénom ">
                     </div>
                     <div class="input-group  mb-3">
                         <span class="input-group-text">
                             <i class="fa fa-envelope">
                             </i>
                         </span>
-                        <input type="text" class="form-control" placeholder="Email ">
+                        <input type="text" class="form-control" name="email" placeholder="Email ">
                     </div>
                     <div class="input-group  mb-3">
                         <span class="input-group-text">
                             <i class="fa fa-lock">
                             </i>
                         </span>
-                        <input type="text" class="form-control" placeholder="Mot de passe ">
+                        <input type="text" class="form-control" name="password" placeholder="Mot de passe ">
                     </div>
                     <div class="d-grid">
-                        <button type="button" class="btn btn-success">S’inscrire</button>
+                        <button type="buton" name="valider" class="btn btn-success">S’inscrire</button>
+                        <p class="text-center">
+                            <i style="color: red">
+                                <?php
+                                if (isset($message)) {
+                                    echo $message;
+                                }
+                                ?>
+                            </i>
+                        </p>
                         <p class="text-center text-muted mt-3">
                             En cliquant sur S’inscrire, vous acceptez nos <a href="#"> Conditions générales</a>, notre <a href=""> Politique de confidentialité </a> et notre <a href="#"> Politique d’utilisation</a> des cookies.
                         </p>
@@ -87,4 +116,3 @@ $conn = null;
 </body>
 
 </html>
-<script src='js/bootstrap.js'></script>
