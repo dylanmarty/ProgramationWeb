7e8e2ab8aa4b6d7d4441b47606a0de0bcb3afea3
Michael ESSOMBA <messomba700@gmail.com>
2025-01-15 19:37:46 +0100

    dossier contenant le javascript

---
+++ js/caroussel.js
@@ -0,0 +1,18 @@
+let currentSlide = 0;
+const slides = document.querySelectorAll('.carousel-item');
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.style.display = i === index ? 'block' : 'none';
+    });
+}
+
+function prevSlide() {
+    currentSlide = (currentSlide > 0) ? currentSlide - 1 : slides.length - 1;
+    showSlide(currentSlide);
+}
+
+function nextSlide() {
+    currentSlide = (currentSlide < slides.length - 1) ? currentSlide + 1 : 0;
+    showSlide(currentSlide);
+} // Afficher la première diapositive au chargement de la page showSlide(currentSlide); 
