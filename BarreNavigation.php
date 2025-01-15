fa04de20bdcdf9e338989ab0e49e510f8858fb21
Michael ESSOMBA <messomba700@gmail.com>
2025-01-15 19:47:14 +0100

    Ajout des icones et debut de responsives

--- BarreNavigation.php
+++ BarreNavigation.php
@@ -13,6 +13,7 @@ if (session_status() == PHP_SESSION_NONE) {
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="css/style.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
   <title>ENSIMgames</title>
 </head>
 
@@ -20,29 +21,37 @@ if (session_status() == PHP_SESSION_NONE) {
   <!-- Menu de navigation -->
   <header class="navbar">
     <h1>ENSIMgames</h1>
+    <div class="menu-icon" onclick="toggleMenu()">&#9776;</div>
     <ul class="nav-links">
-      <li><a href="index.php">Accueil</a></li>
+      <li><a href="index.php"><i class="fas fa-home"></i> Accueil</a></li>
       <!-- Menu déroulant pour les jeux -->
       <li class="dropdown">
-        <a href="#" class="dropbtn">Jeux</a>
+        <a href="#" class="dropbtn"><i class="fas fa-gamepad"></i> Jeux</a>
         <div class="dropdown-content">
-          <a href="AttrapeLesEtoiles.php">Attrape les etoiles </a>
+          <a href="AttrapeLesEtoiles.php">AttrapeLesEtoiles</a>
           <a href="Pong.php">Pong</a>
-        </div>
       </li>
-      <li><a href="Meilleur_score.php">Meilleurs Scores</a></li>
+      </div>
+      </li>
+      <li><a href="Meilleur_score.php"><i class="fas fa-trophy"></i>Meilleurs Scores</a></li>
 
       <?php if (isset($_SESSION['Nom']) && isset($_SESSION['Prenom'])): ?>
         <!-- Si l'utilisateur est connecté -->
-        <li><a href="#">Bienvenue, <?php echo $_SESSION['Prenom'] . " " . $_SESSION['Nom']; ?></a></li>
+        <li><a href="#"><i class="fas fa-user"></i>Bienvenue, <?php echo $_SESSION['Prenom'] . " " . $_SESSION['Nom']; ?></a></li>
         <li><a href="deconnexion.php">Se déconnecter</a></li>
       <?php else: ?>
         <!-- Si l'utilisateur n'est pas connecté -->
-        <li><a href="connexion.php">Connexion</a></li>
-        <li><a href="inscription.php">Inscription</a></li>
+        <li><a href="connexion.php"><i class="fas fa-sign-in-alt"></i>Connexion</a></li>
+        <li><a href="inscription.php"><i class="fas fa-user-plus"></i>Inscription</a></li>
       <?php endif; ?>
     </ul>
   </header>
+  <script>
+    function toggleMenu() {
+      const navLinks = document.querySelector('.nav-links');
+      navLinks.classList.toggle('active');
+    }
+  </script>
 </body>
 
 </html>
