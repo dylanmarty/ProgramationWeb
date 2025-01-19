63a33bd9754e5349f827567487bff77d0406fde9
Michael ESSOMBA <messomba700@gmail.com>
2025-01-19 22:10:58 +0100

    carroussel automatique

--- js/caroussel.js
+++ js/caroussel.js
@@ -16,3 +16,12 @@ function nextSlide() {
     currentSlide = (currentSlide < slides.length - 1) ? currentSlide + 1 : 0;
     showSlide(currentSlide);
 } // Afficher la première diapositive au chargement de la page showSlide(currentSlide); 
+
+const totalSlides = slides.length;
+document.getElementById('next').addEventListener('click', nextSlide);
+document.getElementById('prev').addEventListener('click', prevSlide);
+
+// Auto slide 
+setInterval(nextSlide, 6000);
+// Initialize carousel 
+showSlide(currentSlide);
