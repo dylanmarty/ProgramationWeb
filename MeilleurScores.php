7ce8a2e9216567db29370dd53a507f121b8ca697
Michael ESSOMBA <messomba700@gmail.com>
2025-01-19 17:10:08 +0100

    Ajout Tableau de score

--- MeilleurScores.php
+++ MeilleurScores.php
@@ -12,25 +12,48 @@
 </head>
 
 <body>
-    <div class="container" id="pong">
-        <h1 class="tab">Jeu du Pong</h1>
-        <table>
-            <tbody>
-                <?php
-                include 'TableauScorePong.php';
-                ?>
-            </tbody>
-        </table>
+    <div class="grid-container">
+        <div class="container" id="pong">
+            <h1 class="tab">Jeu du Pong</h1>
+            <table>
+                <tbody>
+                    <?php include 'TableauScorePong.php'; ?>
+                </tbody>
+            </table>
+        </div>
+        <div class="container" id="attrapeEtoile">
+            <h1 class="tab">Jeu de l'attrapeEtoile</h1>
+            <table>
+                <tbody>
+                    <?php include 'TableauScoreAttrapeEtoiles.php'; ?>
+                </tbody>
+            </table>
+        </div>
+        <div class="container" id="snake">
+            <h1 class="tab">Jeu du Snake</h1>
+            <table>
+                <tbody>
+                    <?php include 'TableauScoresnake.php'; ?>
+                </tbody>
+            </table>
+        </div>
+        <div class="container" id="flappy_Bird">
+            <h1 class="tab">Jeu du Flappy Bird</h1>
+            <table>
+                <tbody>
+                    <?php include 'TableauScoreflappy_bird.php'; ?>
+                </tbody>
+            </table>
+        </div>
+        <div class="container" id="j_2048">
+            <h1 class="tab">Jeu du 2048</h1>
+            <table>
+                <tbody>
+                    <?php include 'TableauScore2048.php'; ?>
+                </tbody>
+            </table>
+        </div>
     </div>
-    <div class="container" id="attrapeEtoile">
-        <h1 class="tab">Jeu de l'attrapeEtoile</h1>
-        <table>
-            <tbody>
-                <?php
-                include 'TableauScoreAttrapeEtoiles.php';
-                ?>
-            </tbody>
-        </table>
     </div>
 </body>
 
