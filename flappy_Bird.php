7cd5ed8f2371d6f50c6b4cc3c6757408fda396e3
Michael ESSOMBA <messomba700@gmail.com>
2025-01-19 04:02:16 +0100

    Ajout du jeu flappy_Bird V1

---
+++ flappy_Bird.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="CSS/flappy_Bird.css">
+    <link rel="icon" type="img/png" href="img/favicon.ico">
+    <title>Flappy Bird</title>
+    <?php
+    include 'BarreNavigation.php';  // Inclure la barre de navigation
+    ?>
+</head>
+
+<body>
+    <div class="background"></div>
+    <img src="img/Bird.png" alt="inbvalid-img" class="bird" id="bird-1">
+    <div class="message">
+        Entrer pour commencer le jeu <p><span style="color: red;">&uarr;</span>Flêche haut pour controler</p>
+
+    </div>
+    <div class="score">
+        <span class="score_title"></span>
+        <span class="score_val"></span>
+    </div>
+    <script src="js/flappy_Bird.js"></script>
+</body>
+
+</html>
